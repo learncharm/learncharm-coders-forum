667ebe31d12ddebd023ed21cc95932a93180306a
Avnish Bharadva <avnishbharadva@gmail.com>
2022-05-03 20:41:44 +0530

    added navbar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='container-fluid text-center bg-dark text-light py-3'>
+      <p>Developed By - Avnish Bharadva & Krupesh Vithlani</p>
+    </div>
+  )
+}
